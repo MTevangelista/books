6026496fa6babccab04e23056dc934b17d7473bb
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-08-31 12:15:52 -0300

    feat(server): add mongodb setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
+require("dotenv").config()
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const app = express();
 
@@ -7,7 +10,16 @@ const app = express();
     // Body Parser
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
-//  
+// Mongo
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch((err) => {
+    console.log(`Connection error: ${err}`);
+})
 
 app.get('/', (req, res) => {
     res.json('Hello World')
