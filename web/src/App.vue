38959be3e8df8f71f6dbbe614fcdbc84a3fba32f
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-09-01 11:51:57 -0300

    feat(web): initial setup

---
+++ web/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-container class="text-center mt-5">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </b-container>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+</style>
