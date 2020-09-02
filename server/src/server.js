772e9d8dbd0d56105668f155cbaee7e203280736
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-09-02 11:51:00 -0300

    feat(server): add cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 
@@ -9,6 +10,8 @@ const BookRoute = require('./routes/bookRoute')
 const app = express();
 
 // Settings
+    // Cors
+    app.use(cors())
     // Body Parser
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
