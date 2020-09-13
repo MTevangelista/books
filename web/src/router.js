32b25199683e431222e9dc30922848e1de588c30
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-09-13 12:09:06 -0300

    feat(web): add route to detail a book

--- web/src/router.js
+++ web/src/router.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router';
 import Home from '../src/pages/Home.vue'
 import BookForm from '../src/pages/BookForm.vue'
 import EditBook from '../src/pages/EditBook.vue'
+import BookDetails from '../src/components/BookDetails.vue'
+
 Vue.use(VueRouter);
 
 export default new VueRouter({
@@ -23,6 +25,11 @@ export default new VueRouter({
             path: '/edit-book',
             name: 'EditBook',
             component: EditBook
+        },
+        {
+            path: '/book-details',
+            name: 'BookDetails',
+            component: BookDetails
         },
     ]
 });
